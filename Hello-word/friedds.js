59ff32d374c58e60c1a9554715aebeb6341e0c2e
sushant3975 <mrsushant1234@gmail.com>
2023-03-18 19:56:31 +0530

    Initial cimmit

---
+++ Hello-word/friedds.js
@@ -0,0 +1,23 @@
+let collage = () => {
+    console.log("kolhapur institude of technology , d.y.patil collage of engineering , vivekanand collage of engineering,");
+
+}
+collage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
