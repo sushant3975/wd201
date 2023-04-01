4aa36c6ba7683d4d7ae219acda1b914517559a65
sushant3975 <mrsushant1234@gmail.com>
2023-04-01 19:58:58 +0530

    add es lint and modify code

--- level3/index.js
+++ level3/index.js
@@ -1,7 +1,7 @@
-console.log('sushant');
+console.log('sushant')
 
 // Print message after 100 millisecond
-setTimeout(function() {
-   console.log('patil');
-}, 1);
-console.log('Hello 3');
+setTimeout(function () {
+  console.log('patil')
+}, 1)
+console.log('Hello 3')
